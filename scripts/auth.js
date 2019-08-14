0344848203c0222fa94cab9c0d8e3f9bf81f1dbe
LaupWing <loc_strict@hotmail.com>
2019-08-14 19:42:18 +0200

    starting the auth status

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,3 +24,16 @@ logout.addEventListener('click',(e)=>{
             console.log('user has logged out')
         })
 })
+
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+    auth.signInWithEmailAndPassword(email,password)
+        .then((cred)=>{
+            const modal = document.querySelector('#modal-login')
+            M.Modal.getInstance(modal).close()
+            loginForm.reset()
+        })
+})
