7b3d5d9cdac42b539a5072c76bded71812642a4e
LaupWing <loc_strict@hotmail.com>
2019-08-15 13:52:35 +0200

    added firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context)=>{
+    
+})
