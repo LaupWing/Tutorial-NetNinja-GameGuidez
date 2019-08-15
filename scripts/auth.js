740d8e5e5ededa8e046e55159e781f2c01cce8dd
LaupWing <loc_strict@hotmail.com>
2019-08-15 11:46:19 +0200

    update tutorial

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,39 +1,48 @@
+// Listen for aut states changes
+auth.onAuthStateChanged((user)=>{
+    if(user){
+        console.log('user has logged in', user);
+    }else{
+        console.log('user has logged out');
+    }
+});
+
 const signupForm = document.querySelector('#signup-form')
 signupForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
+    e.preventDefault();
     
     // Get user info
-    const email = signupForm['signup-email'].value
-    const password = signupForm['signup-password'].value
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
 
-    console.log(email, password)
+    console.log(email, password);
     auth.createUserWithEmailAndPassword(email, password)
         .then((cred)=>{
             console.log(cred.user);
-            const modal = document.querySelector('#modal-signup')
-            M.Modal.getInstance(modal).close()
-            signupForm.reset()
-        })
-})
+            const modal = document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
+});
 
 const logout = document.querySelector('#logout')
 logout.addEventListener('click',(e)=>{
-    e.preventDefault()
+    e.preventDefault();
     auth.signOut()
         .then(()=>{
-            console.log('user has logged out')
-        })
-})
+            console.log('user has logged out');
+        });
+});
 
 const loginForm = document.querySelector('#login-form')
 loginForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const email = loginForm['login-email'].value
-    const password = loginForm['login-password'].value
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
     auth.signInWithEmailAndPassword(email,password)
         .then((cred)=>{
-            const modal = document.querySelector('#modal-login')
-            M.Modal.getInstance(modal).close()
-            loginForm.reset()
-        })
-})
+            const modal = document.querySelector('#modal-login');
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
+        });
+});
